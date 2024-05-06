f0f10d780fa4bfbdba87600381aa23b363fa3b51
nadu <nadu@WINDOWS-UINETAB>
2024-05-06 05:30:59 -0700

    Add project files.

---
+++ labsheet 03 question 4/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureTracker week1 = new TemperatureTracker();
+
+            week1.storeTemp();
+            Console.WriteLine();
+            week1.weekReport();
+        }
+    }
+
+    class TemperatureTracker
+    {
+        public double[] temperatureArray = new double[7];
+
+
+        public void storeTemp()
+        {
+            Console.WriteLine("Enter the values of the temperature for this week");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Enter Temperature of day " + (i + 1) + ": ");
+                temperatureArray[i] = Convert.ToDouble(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Successfully stored data!");
+
+        }
+
+        public void weekReport()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Day {i + 1}: {temperatureArray[i]} degrees Celsius");
+
+
+            }
+            Console.WriteLine();
+
+        }
+
+    }
+}
+
+
